bbec2885d5811a03193636dd2e09cf291c6e64f8
Lukáš Lalinský <lukas@oxygene.sk>
2016-02-11 16:01:09 +0100

    2016.02.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mbdata',
-      version='2015.6.20',
+      version='2016.02.11',
       description='MusicBrainz Database Tools',
       author='Lukas Lalinsky',
       author_email='lukas@oxygene.sk',
