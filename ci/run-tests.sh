04bff5e21439fa9a386ce020ef57a51029621284
Lukáš Lalinský <lalinsky@gmail.com>
2021-06-16 11:22:15 +0200

    Fix env name

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,7 +4,7 @@ set -ex
 
 PYTHON_VERSION=$1
 
-MAIN_ENV_NAME=$(echo $PYTHON_VERSION | sed s/\\.//)
+MAIN_ENV_NAME=py$(echo $PYTHON_VERSION | sed s/\\.//)
 FLAKE8_ENV_NAME=$MAIN_ENV_NAME-flake8
 
 export PIP_CACHE_DIR=`pwd`/pip-cache
