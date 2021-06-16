b32bdff94eb559c62c3d941ac5a62933d757156b
Lukáš Lalinský <lalinsky@gmail.com>
2021-06-16 11:21:13 +0200

    Fix version parsing

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -3,8 +3,10 @@
 set -ex
 
 PYTHON_VERSION=$1
-ENV_NAME=$(echo $PYTHON_VERSION | sed s/\.//)
+
+MAIN_ENV_NAME=$(echo $PYTHON_VERSION | sed s/\\.//)
+FLAKE8_ENV_NAME=$MAIN_ENV_NAME-flake8
 
 export PIP_CACHE_DIR=`pwd`/pip-cache
 
-tox --recreate -e $ENV_NAME
+tox --recreate -e $MAIN_ENV_NAME,$FLAKE8_ENV_NAME
